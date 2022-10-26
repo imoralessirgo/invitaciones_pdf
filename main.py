3602c829dc12b3e0c8c5414d089ac1a0b88ce1e6
Isabel Morales Sirgo <imoralessirgo@outlook.com>
2022-10-26 15:09:47 -0500

    init

---
+++ main.py
@@ -0,0 +1,54 @@
+import csv
+from json.tool import main
+from fpdf import FPDF
+
+
+mapa_invitados = {
+    "1" : "BOLETO PERSONAL",
+    "2" : "DOS INVITADOS",
+    "3" : "TRES INVITADOS",
+    "4" : "CUATRO INVITADOS",
+    "5" : "CINCO INVITADOS"
+}
+
+boletos = ""
+invitados = ""
+
+# font_boletos = "Safira March Personal Use"
+# font_invitados = "Assistant Lite"
+
+font_boletos = "Arial"
+font_invitados = "Arial"
+
+lista_1 = "Xime.csv"
+lista_2 = "Hono.csv"
+
+def nuevo_pdf(txt_boletos, txt_invitado):
+    pdf = FPDF("P","mm", [376.76666667,577.58541667])
+    pdf.add_page()
+    pdf.set_font(font_boletos, "", 11)
+    pdf.set_xy(90.4875, 374.65)
+    pdf.set_char_spacing(5)
+    pdf.cell(108, 20, txt = txt_boletos, align = 'C')
+    pdf.set_font(font_invitados, "", 22)
+    pdf.set_xy(89.958333333, 350.30833333)
+    pdf.set_char_spacing(5)
+    with pdf.rotation(90):
+        pdf.cell(295, 20, txt = txt_invitado, align = 'C')
+    pdf.output("./" + txt_invitado + ".pdf")  
+
+
+def pdfs_para_lista(lista):
+    with open(lista) as f:
+        rdr = csv.reader(f, delimiter=",")
+        for row in rdr:
+            nombre = row[3].upper()
+            boletos = mapa_invitados[row[1]]
+            nuevo_pdf(boletos, nombre)
+
+
+def main():
+    nuevo_pdf("Dos Invitados", "Isabel Morales Sirgo")
+
+if __name__=="__main__":
+    main()
